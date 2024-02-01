ddd61acbf1ef006aa92c3afb055231cbbff4c3f8
khagg <kellyahagg@gmail.com>
2024-01-31 20:47:24 -0800

    Player movement forward

---
+++ Endless Runner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //variables made public to allow editing in Unity editor
+    public float speed = 5;
+    public Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
